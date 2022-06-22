6b1d208b5d15ff1b7a1e4a4af54bc12117b930fb
Desiree2018 <107139000+Desiree2018@users.noreply.github.com>
2022-06-22 13:22:41 -0500

    Add files via upload

---
+++ programuno.py
@@ -0,0 +1,20 @@
+##pedir al usuario numeros a y b
+##si a es mayor que b 
+##si b es mayor que a 
+print ("INGRESA UN NUMERO ")
+numa=int (input())
+print("Ingresa otro numero")
+numb=int(input())
+suma = numa + numb
+multi = numa * numb
+if numa >= numb:
+    print("La suma de tus numero es:",suma)
+if numb >= numa:
+    print("La multiplicaion de tus numeros es:", multi)
+
+
+
+
+
+
+
